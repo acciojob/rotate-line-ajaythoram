326f5154fe89a660a76a5a3fb43fa334e85b09e3
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-01 10:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line = document.querySelector('#line');
+
+let angle = 0;
+setInterval(() => {
+  angle += 1;
+  line.style.transform = `rotate(${angle}deg)`;
+}, 10);
+
